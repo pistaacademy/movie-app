f002f9a67ddf1fbbbbac721a8c8d765fa230e4b6
pistaacademy <pista.academy@gmail.com>
2023-09-17 07:13:18 +0330

    Session-7

---
+++ middleware/validator.js
@@ -0,0 +1,17 @@
+const { check, validationResult } = require("express-validator")
+
+exports.userValidator = [
+
+    check("name").trim().not().isEmpty().withMessage("Name is missing!"),
+    check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
+    check("password").trim().not().isEmpty().withMessage("Passowrd is missing")
+    .isLength({ min: 5, max: 20}).withMessage("Password must be 5 to 20 characters long!")
+]
+
+exports.validate = (req, res, next) => {
+    const error = validationResult(req).array();
+    if(error.length){
+        return res.json({error: error[0].msg})
+    }
+    next()
+}
--- routers/user.js
+++ routers/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
+const { userValidator, validate } = require("../middleware/validator");
 const { create } = require("../controller/user");
 
 const router = express.Router();
 
-router.post("/create", create)
+router.post("/create",userValidator,validate,create)
 
 
 
