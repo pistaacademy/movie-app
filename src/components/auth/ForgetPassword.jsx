f396ad55abd05a26dd380a39fdb178e02588aa22
pistaacademy <pista.academy@gmail.com>
2023-10-15 06:26:21 +0330

    Session-25

--- src/components/auth/ForgetPassword.jsx
+++ src/components/auth/ForgetPassword.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Container from '../Container';
+import Title from '../form/Title';
+import FormInput from '../form/FormInput';
+import Submit from '../form/Submit';
+import CustomeLink from '../CustomeLink';
+
 
 export default function ForgetPassword() {
     return (
-        <div>Forget Password</div>
+        <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
+            <Container>
+                <form className='bg-secondary rounded p-6 w-96 space-y-6'>
+                    <Title>Please Enter Your Email</Title>
+                    <FormInput name="email" placeholder="example@gmail.com" label="Email" type="text" autocomplete="off" />
+                    <Submit value="Send Link" />
+
+                    <div className="flex justify-between">
+                        <CustomeLink to="/auth/signin" children="Sign in" />
+                        <CustomeLink to="/auth/signup" children="Sign Up" />
+                    </div>
+                </form>
+            </Container>
+        </div>
     )
 }
