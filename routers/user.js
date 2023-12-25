24fe752fd168a5daf2b6356dca5d741e88f8b0ea
pistaacademy <pista.academy@gmail.com>
2023-12-25 06:52:40 +0330

    Session-45-Backend

--- routers/user.js
+++ routers/user.js
@@ -1,7 +1,10 @@
 const express = require("express");
+
+
 const { userValidator, validate, validatePassword, signInValidator } = require("../middleware/validator");
 const { create,verifyEmail,resendEmailVerificationToken,forgetPassword, sendResetPasswordTokenStatus, resetPassword, signIn } = require("../controller/user");
-const { isValidPassResetToken } = require("../middleware/user")
+const { isValidPassResetToken } = require("../middleware/user");
+const { isAuth } = require("../middleware/auth");
 
 const router = express.Router();
 
@@ -12,5 +15,11 @@ router.post("/resend-email-verification-token",resendEmailVerificationToken)
 router.post("/forget-password",forgetPassword)
 router.post("/verify-pass-reset-token",isValidPassResetToken, sendResetPasswordTokenStatus)
 router.post("/reset-password",validatePassword,isValidPassResetToken,validate, resetPassword)
+
+
+router.get('/is-auth', isAuth, (req, res) => {
+    const { user } = req;
+    res.json({ user: { id: user._id, name: user.name, email: user.email }})
+})
 
 module.exports = router;
