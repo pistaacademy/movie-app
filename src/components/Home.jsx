60f3abc3b67653d61d9cf3a444f8b75f382205f4
pistaacademy <pista.academy@gmail.com>
2024-01-01 06:13:14 +0330

    Session-54-Front

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import CustomeLink from './CustomeLink';
+import { useAuth } from "../hooks";
+import Container  from './Container';
 
 export default function Home() {
+    const { authInfo } = useAuth();
+    const {isLoggedIn} = authInfo;
+    const isVerified = authInfo.profile?.isVerified;
+
     return (
-        <div>
-            <h1>Home Page</h1>
-            <CustomeLink to="/auth/signin" children="Signin" />
-        </div>
+        <Container>
+            {isLoggedIn && !isVerified ? <p className="text-lg text-center bg-blue-50 p-2">
+                It Looks loke you havn't verified your account,{" "}
+                <button className="text-blue-500 font-semibold hover:underline">
+                    click here to verify your account.
+                </button>
+            </p> : null}
+        </Container>
     )
 }
