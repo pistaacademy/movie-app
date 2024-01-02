4876d24f395069c4a9dc05a9d08129304244da12
pistaacademy <pista.academy@gmail.com>
2024-01-02 06:52:52 +0330

    Session-57

--- routers/actor.js
+++ routers/actor.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { createActor } = require("../controller/actor");
+const { uploadImage } = require("../middleware/multer");
+const { actorInfoValidator, validate } = require("../middleware/validator");
 
 const router = express.Router();
 
-router.post('/create', createActor)
+router.post('/create',uploadImage.single('avatar'),actorInfoValidator,validate , createActor)
 
 module.exports = router;
