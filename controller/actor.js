7e85ac8c4d38a0ac0d69e0bd3876111168182d9e
pistaacademy <pista.academy@gmail.com>
2024-01-03 06:20:19 +0330

    Session-58

--- controller/actor.js
+++ controller/actor.js
@@ -1,5 +1,7 @@
+const { isValidObjectId } = require('mongoose');
 const Actor = require('../model/actor');
 const cloudinary = require('cloudinary').v2;
+const { sendError } = require("../utils/helper");
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
@@ -8,9 +10,22 @@ cloudinary.config({
     secure: true
   });
 
-exports.createActor = (req, res) => {
+exports.createActor = async (req, res) => {
     const { name, about, gender } = req.body
     const {file} = req
 
     const newActor = new Actor({name, about, gender})
+    if(file) {
+      const {secure_url, public_id} = await cloudinary.uploader.upload(file.path)
+      newActor.avatar = {url: secure_url, public_id};
+    }
+    await newActor.save()
+    res.status(201).json({
+      id: newActor._id,
+      name,
+      about,
+      gender,
+      avatar: newActor.avatar?.url
+    })
 }
+
--- routers/actor.js
+++ routers/actor.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createActor } = require("../controller/actor");
+const { createActor, updateActor } = require("../controller/actor");
 const { uploadImage } = require("../middleware/multer");
 const { actorInfoValidator, validate } = require("../middleware/validator");
 
 const router = express.Router();
 
 router.post('/create',uploadImage.single('avatar'),actorInfoValidator,validate , createActor)
+
 
 module.exports = router;
