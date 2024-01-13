0c25e532e4f31a29e0c170c38cfdde7a48de792c
pistaacademy <pista.academy@gmail.com>
2024-01-13 06:02:33 +0330

    Session-68

--- middleware/validator.js
+++ middleware/validator.js
@@ -36,11 +36,13 @@ exports.validateMovie = [
         for(let g of value){
             if(!genres.includes(g)) throw Error('Invalid genres!')
         }
+        return true;
     }),
     check('tags').isArray({min: 1}).withMessage('Tags must be an array of strings!').custom((tags) => {
         for(let tag of tags){
             if(typeof tag !== 'string') throw Error('Tags must be an array of strings!')
         }
+        return true;
     }),
     check('cast').isArray().withMessage("Cast must be an array of objects!").custom((cast)=> {
         for(let c of cast){
@@ -48,7 +50,27 @@ exports.validateMovie = [
             if(!c.rolaAs?.trim()) throw Error('Role as is missing inside cast')
             if(typeof c.leadActor !== 'boolean') throw Error('Only accepted boolean value inside leadActor inside cast')
         }
+        return true;
     }),
+    check(trailerInfo).isObject().withMessage('trailerInfo must be an object with url and public_id').custom(({url, public_id}) => {
+        try {
+            const result = new URL(url);
+            if(!result.protocol.includes('http')) throw Error('Trailer url is invalid!')
+
+            const arr = url.split('/')
+            const publicId = arr[arr.length-1].split('.')[0]
+
+            if(public_id !== publicId) throw Error('Trailer public_id is invalid!')
+            return true;
+        }
+        catch{
+            throw Error('Trailer url is invalid!')
+        }
+    }),
+    check('poster').custom((_,{req})=> {
+        if(!req.file) throw Error("Poster file is missing!");
+        return true;
+    })
 ]
 
 exports.validate = (req, res, next) => {
