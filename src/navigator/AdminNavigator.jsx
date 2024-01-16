fd801b01e38c15449d91c10acd86145fcd230a79
pistaacademy <pista.academy@gmail.com>
2024-01-16 06:00:12 +0330

    Session-75

--- src/navigator/AdminNavigator.jsx
+++ src/navigator/AdminNavigator.jsx
@@ -4,14 +4,20 @@ import NotFound from '../components/user/NotFound';
 import Dashboard from '../components/admin/Dashboard';
 import Movies from '../components/admin/Movies';
 import Actors from '../components/admin/Actors';
+import Navbar from '../components/admin/Navbar';
 
 export default function AdminNavigator() {
     return (
-        <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/actors" element={<Actors />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
+        <div className='flex'>
+            <Navbar />
+            <div className="flex-1 p-2 max-w-screen-xl">
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/movies" element={<Movies />} />
+                    <Route path="/actors" element={<Actors />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </div>
+        </div>
     )
 }
