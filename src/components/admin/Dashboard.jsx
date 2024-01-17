f8a6b687bf14be209dbf7cba3e6090b925cdfcbc
pistaacademy <pista.academy@gmail.com>
2024-01-17 06:42:34 +0330

    Session-77

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,31 +1,80 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 
-
 export default function Dashboard() {
-    return <div className='flex items-center justify-between relative'>
-        <input type="text" className='border-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary dark:text-white transition bg-transparent rounded text-lg p-1 outline-none' placeholder='Search Movies...' />
+  const [showOptions, setShowOptions] = useState(false);
 
-        <button className='flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 rounded text-lg px-3 py-1'>
-            <span>Create</span>
-            <AiOutlinePlus />
-        </button>
-        <CreateOptions />
+  return (
+    <div className="flex items-center justify-between relative">
+      <input
+        type="text"
+        className="border-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary dark:text-white transition bg-transparent rounded text-lg p-1 outline-none"
+        placeholder="Search Movies..."
+      />
+      <button
+        onClick={() => setShowOptions(true)}
+        className="flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 rounded text-lg px-3 py-1"
+      >
+        <span>Create</span>
+        <AiOutlinePlus />
+      </button>
+
+      <CreateOptions
+        visible={showOptions}
+        onClose={() => setShowOptions(false)}
+      />
     </div>
-    
+  );
 }
 
-const CreateOptions = () => {
-    return (
-        <div className='absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded'>
-            <Option>Add Movie</Option>
-            <Option>Add Actor</Option>
-        </div>
-    )
-}
+const CreateOptions = ({ visible, onClose }) => {
+  const container = useRef();
+  const containerID = "option-container";
 
-const Option = ({children, onClick }) => {
-    return (
-        <button onClick={onClick} className='dark:text-white text-secondary hover:opacity-80 transition'>{children}</button>
-    )
-}
+  useEffect(() => {
+    const handleClose = (e) => {
+      if (!visible) return;
+      const { parentElement, id } = e.target;
+
+    if(parentElement.id === containerID || id === containerID) return;
+
+      if(container.current) {
+        if(!container.current.classList.contains("animate-scale"))
+            container.current.classList.add("animate-scale-reverse");
+      }
+    }
+
+    document.addEventListener("click", handleClose);
+    return () => {
+      document.removeEventListener("click", handleClose);
+    };
+  }, [visible]);
+
+  if (!visible) return null;
+
+  return (
+    <div
+      id ={containerID}
+      ref={container}
+      className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded animate-scale"
+      onAnimationEnd={(e) => {
+        if(e.target.classList.contains("animate-scale-reverse")) onClose()
+        e.target.classList.remove("animate-scale")
+      }}
+    >
+      <Option>Add Movie</Option>
+      <Option>Add Actor</Option>
+    </div>
+  );
+};
+
+const Option = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="dark:text-white text-secondary hover:opacity-80 transition"
+    >
+      {children}
+    </button>
+  );
+};
